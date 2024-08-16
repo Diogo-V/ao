04fb3757060360a3decddc840641a1a45e5cf82d
Diogo-V <diogo.venancio00@gmail.com>
2024-08-16 20:56:11 +0000

    fix: e2e test

--- test/sparsity/test_marlin.py
+++ test/sparsity/test_marlin.py
@@ -4,7 +4,6 @@ import pytest
 
 from torch import nn
 from torch.testing._internal.common_utils import TestCase, run_tests
-from torchao.utils import TORCH_VERSION_AFTER_2_3
 from torchao.dtypes import MarlinSparseLayoutType
 from torchao.sparsity.sparse_api import apply_fake_sparsity
 from torchao.quantization.quant_api import int4_weight_only, quantize_
@@ -12,10 +11,9 @@ from torchao.quantization.quant_api import int4_weight_only, quantize_
 
 class TestQuantSparseMarlin(TestCase):
 
-    @pytest.mark.skipif(not TORCH_VERSION_AFTER_2_3, reason="pytorch 2.3+ feature")
     @pytest.mark.skipif(not torch.cuda.is_available(), reason="Need CUDA available")
     def test_quant_sparse_marlin_layout(self):
-        input = torch.rand((128, 128)).half().cuda()
+        input = torch.rand((128, 128)).bfloat16().cuda()
         model = (
             nn.Sequential(
                 nn.Linear(128, 256),
@@ -23,16 +21,15 @@ class TestQuantSparseMarlin(TestCase):
                 nn.ReLU(),
                 nn.Linear(256, 128),
             )
-            .half()
+            .bfloat16()
             .cuda()
         )
 
         apply_fake_sparsity(model)
         model_copy = copy.deepcopy(model)
-        model_copy = model_copy.bfloat16()
 
         quantize_(model_copy, int4_weight_only())
-        dense_result = model_copy(input.bfloat16()).to(torch.float16)
+        dense_result = model_copy(input)
 
         quantize_(model, int4_weight_only(layout_type=MarlinSparseLayoutType()))
         sparse_result = model(input)
